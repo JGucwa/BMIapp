31cabadea6feed20e179c31723f54b1a1dfb4413
JGucwa <kubjaxxx@gmail.com>
2024-02-09 11:08:47 +0100

    Dodano klase BMIResult

---
+++ BMIapp/BMIapp/BMIResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMIapp
+{
+    public class BMIResult
+    {
+        public int Id {  get; set; }
+        public string Title {  get; set; }
+        public DateTime Date {  get; set; }
+        public float Weight {  get; set; }
+        public float Height {  get; set; }
+        public bool Gender {  get; set; }
+        public int Score {  get; set; }
+        public float BMI {  get; set; }
+    }
+}
