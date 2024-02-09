ec620a7670fd978aaf5a9f6c6af3a0da6b1db6f6
JGucwa <kubjaxxx@gmail.com>
2024-02-09 11:36:26 +0100

    Dodano fonkcje do obliczania BMIDodano widok strony głównej aplikacjii

--- BMIapp/BMIapp/MainPage.xaml.cs
+++ BMIapp/BMIapp/MainPage.xaml.cs
@@ -14,5 +14,40 @@ namespace BMIapp
         {
             InitializeComponent();
         }
+        void CalculateBMI(object sender, EventArgs e)
+        {
+            float result = float.Parse(weight.Text) / (float.Parse(height.Text)/100 * float.Parse(height.Text)/100) ;
+
+            Result.Text = result.ToString("F2") + " BMI";
+            if(result <= 18.5f)
+            {
+                Result.Text += "\n Masz niedowage";
+            }
+            else
+            {
+                if (result <= 24.9f)
+                {
+                    Result.Text += "\n Masz prawidłową wagę";
+                }
+                else
+                {
+                    if (result <= 29.9f)
+                    {
+                        Result.Text += "\n Masz nadwagę";
+                    }
+                    else
+                    {
+                        if (result <= 34.9f)
+                        {
+                            Result.Text += "\n Masz poważną niedowage";
+                        }
+                        else
+                        {
+                            Result.Text += "\n Masz otyłość";
+                        }
+                    }
+                }
+            }
+        }
     }
 }
